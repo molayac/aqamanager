92ef7cb6175550070254282ced2e16b770f2b5b3
PC-MOLAYA\Marlon Olaya <olaya.marlon@gmail.com>
2017-04-30 23:59:50 -0500

    Gentelella AqaMan

---
+++ imports/ui/layouts/base/base.js
@@ -0,0 +1,5 @@
+import './base.html';
+import '../../partials/footer.html';
+import '../../partials/menu_profile.html';
+import '../../partials/side_bar_menu.html';
+import '../../partials/top_navigation.html';
---
+++ imports/ui/partials/all_partials.js
@@ -0,0 +1,4 @@
+import './footer.html';
+import './menu_profile.html';
+import './side_bar_menu.html';
+import './top_navigation.html';
